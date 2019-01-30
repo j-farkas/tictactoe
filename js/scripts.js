80933f39fde9be450a167b310e8e38cd744c12c1
Jared Farkas and Randee Layosa <jared@>
2019-01-30 13:52:40 -0800

    Finished game. Win check works, and returns winner id.

--- js/scripts.js
+++ js/scripts.js
@@ -8,11 +8,10 @@ var player1 = new Player("X");
 player1.active = true;
 var player2 = new Player("O");
 
-
 var gameboard =
-  [0,0,0
-  ,0,0,0,
-  0,0,0];
+  [0,1,2
+  ,3,4,5,
+  6,7,8];
 
 const winCombos = [
 [0,1,2],
@@ -25,44 +24,59 @@ const winCombos = [
 [2,4,6]
 ]
 
-
-function swapActive(){
-  if(player1.active === true)
-  {
+function swapActive() {
+  if(player1.active === true) {
     player1.active = false;
     player2.active = true;
-  }else{
+  } else {
     player1.active = true;
     player2.active = false;
   }
 }
 
 function activePlayer (){
-  if(player1.active === true){
+  if(player1.active === true) {
     return player1;
   }
-  return player2;
+    return player2;
 }
 
-
-
-
-function checkWin(id){
-  return winCombos.filter(function(arr){
-    if(arr.includes(id)){
+function checkWin(id) {
+  var wins = winCombos;
+  wins = wins.filter(function(arr) {
+    if(arr.includes(parseInt(id))) {
       return true;
-    }else{return false;}
+    } else {
+      return false;
+    }
   });
+
+  var winner = false;
+   wins.forEach(function(arr) {
+    if(gameboard[arr[0]] === gameboard[arr[1]] && gameboard[arr[0]] === gameboard[arr[2]]) {
+      winner = true;
+    }
+  })
+  return winner;
 }
 
+var count = 0;
 function attachContactListeners() {
   $(".container").on("click", ".col-md-4", function() {
-    console.log($(this).attr("id"));
     var len = $("." + $(this).attr("id")).text().length;
     if(len < 1){
-    $("." + $(this).attr("id")).text(activePlayer().letter);
-    console.log(checkWin($(this).attr("id")));
-    swapActive();
+      count++;
+      $("." + $(this).attr("id")).text(activePlayer().letter);
+      gameboard[$(this).attr("id")] = activePlayer().letter;
+
+      if(checkWin($(this).attr("id")) === true){
+        $("p").text(activePlayer().letter + " wins");
+      }else{
+        if(count === 9){
+          $("p").text("Nobody wins");
+        }
+      }
+      swapActive();
     }
   });
 };
